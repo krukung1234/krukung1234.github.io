acfb902c4087d710577fae10b7a8c8488a67ff81
krukung1234 <thunyaporn.sae1234@gmail.com>
2026-06-29 17:38:31 +0700

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // วางลิงก์ Apps Script /exec ตรงนี้
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbzjV6IWOQJedG3ZnujlgQvij6puPIBYvCtvGnny05xwb6N7_acU3UPEx83CIik1uV0mQQ/exec";
 
 // เปลี่ยนรหัส Admin ตรงนี้
-const ADMIN_PASSWORD = "1234";
+const ADMIN_PASSWORD = "vip69";
